9b310140dfc9df917856ab853ff54319d8b28563
AhmedInes <ahmed.bouzid.ines@gmail.com>
2024-04-17 01:03:55 +0200

    chore: create  guard  in use it in modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,14 +6,15 @@ import { UserCrudComponent } from './user-crud/user-crud.component';
 import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { ProductComponent } from '../components/product/product.component';
+import { AdminAuthGuardService } from '../shared/services/auth-guard.service';
 
 
 // admin routes
 const routes:Routes = [
   {path:'admin-login' , component:LoginComponent},
-  {path:'admin-dashboard' , component:AdminDashboardComponent},
-  {path:'admin-user' , component:UserCrudComponent},
-  {path:'product' , component:ProductComponent},
+  {path:'admin-dashboard'  ,canActivate:[AdminAuthGuardService], component:AdminDashboardComponent},
+  {path:'admin-user'  ,canActivate:[AdminAuthGuardService], component:UserCrudComponent},
+  {path:'product'  ,canActivate:[AdminAuthGuardService], component:ProductComponent},
 ]
 
 @NgModule({
--- src/app/shared/services/auth-guard.service.ts
+++ src/app/shared/services/auth-guard.service.ts
@@ -1,9 +1,105 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree ,ActivatedRouteSnapshot,RouterStateSnapshot} from '@angular/router';
+import { Observable } from 'rxjs';
+
+// Admin before login check
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuardLogin implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    let role = sessionStorage.getItem("role");
+    if (role === 'admin') {
+      return this.router.parseUrl('/admin/admin-dashboard');
+    } else {
+      return true;
+    }
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AdminAuthGuardService {
 
-  constructor() { }
+  // Admin after login check
+  constructor(private router :Router) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    let role = sessionStorage.getItem("role") ;
+    if(role =='admin') {
+      return true
+    }else {
+      this.router.navigateByUrl('/admin/admin-login')
+      return false
+    }
+
+  }
+}
+
+
+// Customer buyer and seller before check
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerBuyerAuthGuardLogin implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
+    let role = sessionStorage.getItem("role");
+    if (role === 'seller') {
+      return this.router.parseUrl('/customer/seller-dashboard');
+    } else if (role === 'buyer') {
+      return this.router.parseUrl('/customer/buyer-dashboard');
+    } else {
+      return true;
+    }
+  }
+}
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuyerAuthGuardService {
+
+  // Buyer after login check
+  constructor(private router :Router) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    let role = sessionStorage.getItem("role") ;
+    if(role =='buyer') {
+      return true
+    }else {
+      this.router.navigateByUrl('/customer/signin')
+      return false
+    }
+
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SellerAuthGuardService {
+
+  // Seller after login check
+  constructor(private router :Router) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+    let role = sessionStorage.getItem("role") ;
+    if(role =='seller') {
+      return true
+    }else {
+      this.router.navigateByUrl('/customer/signin')
+      return false
+    }
+
+  }
 }
