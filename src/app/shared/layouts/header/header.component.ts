74fba597ccd232fbb467004bb9eead3ac3478e26
AhmedInes <ahmed.bouzid.ines@gmail.com>
2024-04-17 01:26:36 +0200

    chore: controling the header with  users roles

--- src/app/shared/layouts/header/header.component.ts
+++ src/app/shared/layouts/header/header.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  logged_in : boolean = false;
+  language:string ='english';
+  user_role :string | null = '' ;
 
+  constructor( private router : Router){}
+  ngOnInit(): void {
+  }
   switchLanguage(language:string) {
 
+  }
+  ngDoCheck() {
+    this.user_role = sessionStorage.getItem('role');
+    const user_session_id = sessionStorage.getItem('user_session_id') ;
+    if(user_session_id) {
+      this.logged_in = true ;
+    }
+  }
+
+  logout() {
+    sessionStorage.removeItem('user_session_id');
+    sessionStorage.removeItem('role');
+    this.router.navigateByUrl('customer/signin') ;
+    location.reload() ;
   }
 }
