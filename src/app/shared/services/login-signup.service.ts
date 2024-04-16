b22bac08c7316a1c258c5b4b8bbdd80c385e25ba
AhmedInes <ahmed.bouzid.ines@gmail.com>
2024-04-17 01:01:38 +0200

    chore: auth service

--- src/app/shared/services/login-signup.service.ts
+++ src/app/shared/services/login-signup.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from 'src/app/core/service/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginSignupService {
 
-  constructor() { }
+  URL = "http://localhost:3000";
+
+  constructor(private http :HttpClient , private apiService : ApiService) { }
+
+  authLogin(username:string , password : string) : Observable<any> {
+    return this.http.get(this.URL+'/user?email='+username+'&password='+password);
+  }
+  userRegistre(user_detail : any) : Observable<any> {
+    return this.http.post(this.URL+'/user',user_detail);
+  }
+  adminLogin(username:string , password : string) : Observable<any> {
+    return this.apiService.get(this.URL+'/user?email='+username+'&password='+password+'&role=admin');
+  }
 }
