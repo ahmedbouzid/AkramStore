26cd02741c7a1b0b43c39a5d733f56d3e10c6534
AhmedInes <ahmed.bouzid.ines@gmail.com>
2024-04-18 23:15:27 +0200

    chore : create services

--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http : HttpClient , private router:Router) { }
+  URL = 'http://localhost:3000/' ;
+  singleProductID = new BehaviorSubject(null) ;
+  currentProduct = this.singleProductID.asObservable() ;
+
+
+  allProduct():Observable<any> {
+    return this.http.get(this.URL+'products') ;
+  }
+
+  quickByProduct(produc_id:any) {
+    if(produc_id) {
+      return this.singleProductID.next(produc_id) ;
+    }
+    }
+  individualProduct(product_id : any) {
+    return this.http.get(this.URL + 'products/'+product_id)
+  }
+  userDetails(id : any) {
+    return this.http.get(`${this.URL}user/${id}`)
+  }
+  inserNewOrder(orderDto :any) {
+    return this.http.post(this.URL+'orders/' , orderDto)
+  }
+  orderDashboardData() : Observable<any>{
+    return this.http.get(this.URL + 'orders/') ;
+  }
+  orderDashboardDetails(): Observable<any> {
+    return this.http.get(this.URL +'products/')
+  }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,68 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, Observable } from 'rxjs';
+import { User } from 'src/app/core/model/user-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  URL = "http://localhost:3000/";
+
+  constructor(private http : HttpClient, private  router:Router) { }
+  userDashboaedData() {
+    return this.http.get(this.URL + 'user/')
+  }
+
+
+  oneUser(id: any) {
+    return this.http.get<any>(`${this.URL}user/${id}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          // Handle 404 error (user not found) appropriately
+          console.log('User not found');
+        } else {
+          // Handle other errors
+          console.error('Error fetching user:', error);
+        }
+        throw error; // Rethrow the error for the component to handle
+      })
+    );
+  }
+
+
+  updateUser(id:any , dto : any) :Observable<any>{
+    return this.http.put(this.URL+ 'user/'+id, dto)
+    .pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          // Handle 404 error (user not found) appropriately
+          console.log('User not found');
+        } else {
+          // Handle other errors
+          console.error('Error fetching user:', error);
+        }
+        throw error; // Rethrow the error for the component to handle
+      })
+    );
+
+  }
+  deleteUser(id : any) {
+    return this.http.delete(`${this.URL}user/${id}`).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 404) {
+          // Handle 404 error (user not found) appropriately
+          console.log('User not found');
+        } else {
+          // Handle other errors
+          console.error('Error fetching user:', error);
+        }
+        throw error; // Rethrow the error for the component to handle
+      })
+    )
+  }
+
+
 }
